6a5e7452e8a553f2c558066ab146cd5d3c923fb0
Anton Evangelatov <anton.evangelatov@gmail.com>
2023-04-06 13:13:28 +0200

    fix metrics endpoint

--- extern/boostd-data/bench/main.go
+++ extern/boostd-data/bench/main.go
@@ -69,7 +69,7 @@ func metricsSetup() {
 	tags := make(map[string]string)
 	tags["host"] = hostname
 
-	endpoint := "http://10.14.1.217:8086"
+	endpoint := "http://10.14.1.79:8086"
 	username := "admin"
 	password := "admin"
 	database := "metrics"
